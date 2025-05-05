8246c52c63d4fdc0b3cecf4a747f688e2a4d0b63
eemaraa <eyademarah@gmail.com>
2025-05-06 02:09:04 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 10000;
+const wss = new WebSocket.Server({ port: PORT });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    // بث الرسالة لجميع العملاء الآخرين
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+console.log('WebSocket server is running on port', PORT); 
